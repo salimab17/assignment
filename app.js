dfc10ea5a0baa03e4e97f3670b4d4cae18920d31
salim <s19bsit004@student.ug.edu.pk>
2022-02-16 13:32:34 +0500

    contact comment

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('about')
 })
+app.get('/contact', (req, res) => {
+  res.send('contact!')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
