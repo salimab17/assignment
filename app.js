53ae94f40d12f301b45fdb933e9be6059dba4b6b
salimab17 <99792793+salimab17@users.noreply.github.com>
2022-03-02 18:50:03 +0500

    Update app.js
    views

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hi Salim!')
+  res.render('home');
 })
 app.get('/about', (req, res) => {
-  res.send('about')
-})
-app.get('/contact', (req, res) => {
-  res.send('contact!')
-})
+    res.render('about');s
+  })
+  app.get('/contact', (req, res) => {
+    res.render('contact');s
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
